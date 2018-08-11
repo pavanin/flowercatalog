3333b86e31ccbb95b609c2769c20d8d1b36235bc
Pavani Neduri <pavanin@In-Pavanin.local>
2018-08-11 16:03:12 +0530

    kept build files in master branch

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
-import GuestBook from "./components/GuestBook";
-
-ReactDOM.render(<GuestBook />, document.getElementById("root"));
